2c1642cc0e2eac3ffc8a6e9c50ee1cb85648a692
Sergio.Municio <sergio.municio@geograma.com>
2018-04-18 13:30:47 +0200

    24 - Bonus: Working with NgRx in our Project (332-auth-effects-finished)

--- ngrx-basics-start/src/app/auth/store/auth.actions.ts
+++ ngrx-basics-start/src/app/auth/store/auth.actions.ts
@@ -1,9 +1,23 @@
 import { Action } from '@ngrx/store';
 
+export const TRY_SIGNUP = 'TRY_SIGNUP';
+export const TRY_SIGNIN = 'TRY_SIGNIN';
 export const SIGNUP = 'SIGNUP';
 export const SIGNIN = 'SIGNIN';
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
+
+export class TrySignup implements Action {
+    readonly type = TRY_SIGNUP;
+
+    constructor(public payload: { username: string, password: string }) { }
+}
+
+export class TrySignin implements Action {
+    readonly type = TRY_SIGNUP;
+
+    constructor(public payload: { username: string, password: string }) { }
+}
 
 export class Signup implements Action {
     readonly type = SIGNUP;
@@ -27,4 +41,6 @@ export type AuthActions =
     Signup |
     Signin |
     Logout |
-    SetToken;
+    SetToken |
+    TrySignup |
+    TrySignin;
--- ngrx-basics-start/src/app/core/header/header.component.ts
+++ ngrx-basics-start/src/app/core/header/header.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 // import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { DataStorageService } from '../../shared/data-storage.service';
-import { AuthService } from '../../auth/auth.service';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import * as fromApp from '../../store/app.reducers';
 import * as fromAuth from '../../auth/store/auth.reducers';
+import * as AuthActions from '../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -17,7 +17,6 @@ export class HeaderComponent implements OnInit {
   authState: Observable<fromAuth.State>;
 
   constructor(private dataStorageService: DataStorageService,
-    private authService: AuthService,
     private store: Store<fromApp.AppState>) {
   }
 
@@ -39,6 +38,6 @@ export class HeaderComponent implements OnInit {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
